fa3bc0815d9a8b4c2254d2b00903630bd3e9a6c3
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2015-12-03 09:13:05 +0100

    letter grid: init commit

--- app/scripts/controllers/example.js
+++ app/scripts/controllers/example.js
@@ -8,10 +8,6 @@
  * Controller of the qlocktwoAngularApp
  */
 angular.module('qlocktwoAngularApp')
-  .controller('ExampleCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ExampleCtrl', function (letterGridService) {
+    this.grid = letterGridService.grid;
   });
--- app/scripts/services/lettergridservice.js
+++ app/scripts/services/lettergridservice.js
@@ -10,4 +10,14 @@
 angular.module('qlocktwoAngularApp')
   .service('letterGridService', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var grid = ['NSEVENFOURC', 'FELEVENOTWO', 'TWELVEFIVEV', 'TEIGHTENONE', 'TNINETHREEL',
+    'SIXHALFIFTY', 'EFORTYYPAST', 'OFTHIRTYTEN', 'TOTWENTYSIX', 'TWCLOCKFIVE', 'FIFTEENTONE'];
+
+    // Convert to array of arrays
+    var finalGrid = grid.map(function(row){
+      var chars = row.split('').map(function(d){return {char: d, selected: Math.random() > 0.5}});
+      return chars;
+    });
+
+    return {grid: finalGrid};
   });
