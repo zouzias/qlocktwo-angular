1b6a6786c1f1ba20b33f68dac89a8ab373cc2b5e
Anastasios Zouzias <zouzias@gmail.com>
2015-12-17 19:19:15 +0100

    timetophrases: add comments

--- app/scripts/services/timetophrases.js
+++ app/scripts/services/timetophrases.js
@@ -13,6 +13,7 @@
 angular.module('qlocktwoAngularApp')
   .service('TimeToPhrases', function () {
 
+    // Here put your words for hours
     var hours = [{hour: 1, word: 'one'},
                  {hour: 2, word: 'two'},
                  {hour: 3, word: 'three'},
@@ -24,16 +25,30 @@ angular.module('qlocktwoAngularApp')
                  {hour: 9, word: 'nine'},
                  {hour: 10, word: 'ten'},
                  {hour: 11, word: 'eleven'},
-                 {hour: 12, word: 'twelve'}];
+                 {hour: 12, word: 'twelve'},
+                  {hour: 13, word: 'one'},
+                  {hour: 14, word: 'two'},
+                  {hour: 15, word: 'three'},
+                  {hour: 16, word: 'four'},
+                  {hour: 17, word: 'five'},
+                  {hour: 18, word: 'six'},
+                  {hour: 19, word: 'seven'},
+                  {hour: 20, word: 'eight'},
+                  {hour: 21, word: 'nine'},
+                  {hour: 22, word: 'ten'},
+                  {hour: 23, word: 'eleven'},
+                  {hour: 24, word: 'twelve'}];
 
-    var minutes = [{minute: 0, word: 'clock'},
-                  {minute: 5, word: 'five'},
-                  {minute: 10, word: 'ten'},
-                  {minute: 15, word: 'fifteen'},
-                  {minute: 20, word: 'twenty'},
-                  {minute: 30, word: 'thirty'},
-                  {minute: 40, word: 'forty'},
-                  {minute: 50, word: 'fifty'}];
+    // Here put your word for minutes
+    // This will be displayed if time is in interval [start, end]
+    var minutes = [{start: 0, end:  5, word: 'clock'},
+                  {start:  5, end:  9, word: 'five'},
+                  {start: 10, end: 14, word: 'ten'},
+                  {start: 15, end: 19, word: 'fifteen'},
+                  {start: 20, end: 29, word: 'twenty'},
+                  {start: 30, end: 39, word: 'thirty'},
+                  {start: 40, end: 49, word: 'forty'},
+                  {start: 50, end: 59, word: 'fifty'}];
 
     return {hours: hours, minutes: minutes};
   });
