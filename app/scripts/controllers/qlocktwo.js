3a2aea8f37651178c96a9448dc810c0217e97d61
Anastasios Zouzias <zouzias@gmail.com>
2015-12-17 19:20:32 +0100

    controllers: major refactoring

--- app/scripts/controllers/qlocktwo.js
+++ app/scripts/controllers/qlocktwo.js
@@ -11,8 +11,6 @@ angular.module('qlocktwoAngularApp')
   .controller('QlocktwoCtrl', function ($rootScope, $scope, LetterGridService, CurrentTimeService) {
 
     $scope.grid = LetterGridService.grid;
-    var numRows = LetterGridService.numRows;
-    var numCols = LetterGridService.numCols;
 
     var hours = CurrentTimeService.hours.map(function(d){return d.word;});
     var minutes = CurrentTimeService.minutes.map(function(d){return d.word;});
@@ -29,17 +27,37 @@ angular.module('qlocktwoAngularApp')
           $scope.currentTime = CurrentTimeService.now;
         });
 
-        $scope.next();
+        $scope.updateTimePhrases();
         console.log('Updated time is ' + $scope.currentTime);
     });
 
-    /**
-    * Update letter time
-    */
-    $scope.updateTime = function(){
-      $scope.currentHour = hours[CurrentTimeService.indexHour];
-      $scope.currentMinute = minutes[CurrentTimeService.indexMinute];
+    $scope.updateTimePhrases = function(){
+
+      var currentHour = $scope.currentTime.getHours();
+      var currentMinute = $scope.currentTime.getMinutes();
+
+      //console.log('Hour: ' + currentHour)
+      //console.log('Minute: ' + currentMinute)
+
+      $scope.currentHour = CurrentTimeService.hours.filter(function(d) {
+        return d.hour === currentHour;
+      }).map(function(d){return d.word;}).shift();
+
+      console.log('CurrentTimeService hours is  ' + CurrentTimeService.hours[currentHour]);
+      $scope.currentMinute = CurrentTimeService.minutes.filter(function(d){
+        return d.start <= currentMinute && currentMinute <= d.end;
+      }).map(function(d){return d.word;}).shift();
+
+      //console.dir($scope.currentHour);
+      //console.dir($scope.currentMinute);
+
+      $scope.resetGrid(); // Reset Grid
+
+      // Highlight phrase on letter grid
+      var loc = $scope.highlightWord($scope.currentHour, 0, 0);
+      $scope.highlightWord($scope.currentMinute, loc.row, loc.col);
     };
+
 
     /**
     * Resets letter grid
@@ -48,21 +66,21 @@ angular.module('qlocktwoAngularApp')
       LetterGridService.grid = LetterGridService.grid.map(function(d){
         return d.map(function(l){
           return {char: l.char, selected: false};
-        })
-      })
+        });
+      });
 
       $scope.grid = LetterGridService.grid;
     };
 
     /**
-    *
-    * searchGrid:
-    * word: Word to search on letter grid
-    * fromRow: Row index to start the search
-    * fromCol: Column index to start search
-    *
-    * Return a list
-    */
+     * Searches letter grid for a word starting from position (fromRow, fromCol)
+     *
+     * @param searchGrid The letter grid as an array of Strings (each string is a row)
+     * @param word Query word on the letter grid
+     * @param fromRow Start row location (0-indexed)
+     * @param fromCol Start column location (0-indexed)
+       * @returns {*} Array of objects: {row: XXX, col: XXX, sz: XXX} containing locations of found locations
+       */
     $scope.findWord = function(searchGrid, word, fromRow, fromCol){
       var locations = searchGrid.map( function (row, i) {
                                   var col = row.indexOf(word.toUpperCase());
@@ -70,61 +88,45 @@ angular.module('qlocktwoAngularApp')
                                 }).filter( function(d){
                                   return d.col !== -1;  // Remove not found rows
                                 }).filter( function(d){
-                                    return d.row > fromRow || (d.row == fromRow && d.col >= fromCol);
+                                    return d.row > fromRow || (d.row === fromRow && d.col >= fromCol);
                                 });
       return locations;
     };
 
     /**
-    *  Highlights a single word from a (fromRow, fromCol) location
-    *
-    * Returns a new {row: rowIndex, col: colIndex} to continue highlight search
-    */
+     * Highlights a single word from a given (fromRow, fromCol) location.
+     *
+     * It updates the state of Service LetterGridService.grid
+     *
+     * @param word Query word to search on letter grid
+     * @param fromRow Row index to start search
+     * @param fromCol Column index to start search (given about row index)
+       * @returns {*} A location {row: XXX, col: XXX} from which the following possible word can be searched
+       */
     $scope.highlightWord = function(word, fromRow, fromCol){
       var locations = $scope.findWord(LetterGridService.searchGrid, word, fromRow, fromCol);
 
-      if ( locations.length > 0){
-        var foundRow = locations[0].row;
-        var foundCol = locations[0].col;
-        var sz = locations[0].sz;
-
-        for( var j = 0; j < sz; j++){
-          LetterGridService.grid[foundRow][foundCol + j].selected = true;
-        }
-
-        $scope.grid = LetterGridService.grid; // Update grid
-
-        // Check if the column index overflowed
-        if ( foundCol + sz >= numCols ){
-          return {row: foundRow + 1, col: 0};
-        }
-        else{
-          return {row: foundRow, col: foundCol + sz};
-        }
+      if (locations.length === 0) {
+        return {row: fromRow, col: fromCol};
       }
 
-      return {row: fromRow, col: fromCol};
-    };
+      var foundRow = locations[0].row;
+      var foundCol = locations[0].col;
+      var sz = locations[0].sz;
 
-    $scope.previous = function(){
-      $scope.resetGrid();   // Reset letter grid
+      // Update letter grid
+      for( var j = 0; j < sz; j++){
+        LetterGridService.grid[foundRow][foundCol + j].selected = true;
+      }
 
-      CurrentTimeService.previousTime();  // Previous time
-      $scope.updateTime();
+      // Update controllers grid
+      $scope.grid = LetterGridService.grid;
 
-      // Highlight letter grid
-      var loc = $scope.highlightWord($scope.currentHour, 0, 0);
-      $scope.highlightWord($scope.currentMinute, loc.row, loc.col);
-    };
-
-    $scope.next = function(){
-      $scope.resetGrid(); // Reset Grid
-
-      CurrentTimeService.nextTime();  // Next time
-      $scope.updateTime();
-
-      // Highlight phrase on letter grid
-      var loc = $scope.highlightWord($scope.currentHour, 0, 0);
-      $scope.highlightWord($scope.currentMinute, loc.row, loc.col);
+      // Check if the column index overflowed
+      if ( foundCol + sz >= LetterGridService.numCols ) {
+        return {row: foundRow + 1, col: 0};
+      } else{
+        return {row: foundRow, col: foundCol + sz};
+      }
     };
   });
