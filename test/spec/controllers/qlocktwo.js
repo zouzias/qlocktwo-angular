f42123ce52470a952ac2fdfd8156a5d054ca22bf
Anastasios Zouzias <zouzias@gmail.com>
2015-12-26 15:19:10 +0100

    test: fix tests

--- test/spec/controllers/qlocktwo.js
+++ test/spec/controllers/qlocktwo.js
@@ -18,6 +18,6 @@ describe('Controller: QlocktwoCtrl', function () {
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(QlocktwoCtrl.awesomeThings.length).toBe(3);
+    expect(scope.grid.length).toBe(11);
   });
 });
--- test/spec/services/lettergridservice.js
+++ test/spec/services/lettergridservice.js
@@ -1,18 +1,19 @@
 'use strict';
 
-describe('Service: letterGridService', function () {
+describe('Service: LetterGridService', function () {
 
   // load the service's module
   beforeEach(module('qlocktwoAngularApp'));
 
   // instantiate service
   var letterGridService;
-  beforeEach(inject(function (_letterGridService_) {
-    letterGridService = _letterGridService_;
+  beforeEach(inject(function (_LetterGridService_) {
+    letterGridService = _LetterGridService_;
   }));
 
-  it('should do something', function () {
+  it('should contain a grid of String with length 11', function () {
     expect(!!letterGridService).toBe(true);
+    expect(letterGridService.numRows).toBe(11);
   });
 
 });
